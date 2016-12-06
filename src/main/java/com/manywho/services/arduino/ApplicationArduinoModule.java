cade2bf1210a0898d42ad108e2c05e84e0221945
jose <jose.collazzi@manywho.com>
2016-12-06 17:59:11 +0000

    remove s3 copies files

--- src/main/java/com/manywho/services/arduino/ApplicationArduinoModule.java
+++ src/main/java/com/manywho/services/arduino/ApplicationArduinoModule.java
@@ -2,7 +2,7 @@ package com.manywho.services.arduino;
 
 import com.google.inject.AbstractModule;
 
-public class ApplicationS3Module extends AbstractModule {
+public class ApplicationArduinoModule extends AbstractModule {
 
     @Override
     protected void configure() {
--- src/main/java/com/manywho/services/arduino/ServiceConfiguration.java
+++ src/main/java/com/manywho/services/arduino/ServiceConfiguration.java
@@ -1,27 +1,7 @@
 package com.manywho.services.arduino;
 
-import com.manywho.sdk.api.ContentType;
 import com.manywho.sdk.services.configuration.Configuration;
 
 public class ServiceConfiguration implements Configuration {
-    @Configuration.Setting(name="Bucket Name", contentType = ContentType.String)
-    private String bucketName;
 
-    @Configuration.Setting(name="AWS Access Key Id", contentType = ContentType.String)
-    private String awsAccessKeyId;
-
-    @Configuration.Setting(name="AWS Secret Access Key", contentType = ContentType.String)
-    private String awsSecretAccessKey;
-
-    public String getBucketName() {
-        return bucketName;
-    }
-
-    public String getAwsAccessKeyId() {
-        return awsAccessKeyId;
-    }
-
-    public String getAwsSecretAccessKey() {
-        return awsSecretAccessKey;
-    }
 }
